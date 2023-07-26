e18ea7d76e960906d284f31666c0cb3747fd8f2b
Saian <ifsi.seraffin@gmail.com>
2023-07-26 21:57:08 +0200

    importation des datas

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,26 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        const location = document.createElement('p');
+        location.textContent = `${city} ${country}`;
+        const desc = document.createElement('p');
+        desc.textContent = tagline;
+        const cost = document.createElement('p');
+        cost.textContent = `${price}€/jour`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(desc);
+        article.appendChild(cost);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, id,  getUserCardDOM }
 }
